9d38f4fd56698f008bb060786e97b9acb0f02bc8
vale <valerio.modugno@gmail.com>
2015-03-17 19:55:22 +0100

    added eps-greedy

---
+++ +RL/EpsGreedy.m
@@ -0,0 +1,20 @@
+classdef EpsGreedy
+   
+   properties
+    
+   end
+   
+   
+   methods
+      function obj = EpsGreedy()
+      
+       disp('test') 
+        
+      end
+      
+      
+     
+      
+   end
+   
+end
